b91b254451716397d3444196f6c743123de5ab86
Neeraj-Bhattarai <neerajbhttri@gmail.com>
2023-05-07 18:18:31 +0545

    Error fix (Get user)

--- routes/auth.js
+++ routes/auth.js
@@ -102,7 +102,7 @@ router.post("/getuser", fetchuser, async (req, res) => {
   try {
     userId = req.user.id;
     const user = await User.findById(userId).select("-password");
-    res.json(user);
+    res.send(user);
   } catch (error) {
     console.error(error.message);
     res.status(500).send("Internal Server Error");
